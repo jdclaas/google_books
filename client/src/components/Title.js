2e7bfd3e5b41b01d5d4fa7f28f5e2d87ef5a6b0e
jdclaas <jd_claas@icloud.com>
2019-09-20 18:36:30 -0500

    final project

---
+++ client/src/components/Title.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Title() {
+    return (
+        <div className="row ">
+            <div className="col-md-8 mx-auto">
+                <div className="jumbotron">
+                    <h1 className="text">Book Search!</h1>
+                    <p className="text">Look for Books, Save some Books.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Title;
